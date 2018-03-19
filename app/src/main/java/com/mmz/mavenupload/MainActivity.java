313486b7c01c135e2c5f28bf4e281e82789ae9ac
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-03-19 14:41:46 +0800

    Add local repo

--- app/src/main/java/com/mmz/mavenupload/MainActivity.java
+++ app/src/main/java/com/mmz/mavenupload/MainActivity.java
@@ -3,11 +3,16 @@ package com.mmz.mavenupload;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.mmz.localrepo.LocalRepo;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //test local repo
+        LocalRepo.localrepo();
     }
 }
---
+++ localrepo/src/main/java/com/mmz/localrepo/LocalRepo.java
@@ -0,0 +1,13 @@
+package com.mmz.localrepo;
+
+import android.util.Log;
+
+/**
+ * Created by mamingzhang on 2018/3/19.
+ */
+
+public class LocalRepo {
+    public static void localrepo() {
+        Log.v("mmz", "local repo");
+    }
+}
